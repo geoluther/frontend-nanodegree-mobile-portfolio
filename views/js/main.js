6ed646a134f90a8d26222c7085fbe7df13c925d0
George <my latest commit>
2015-02-15 17:18:14 -0700

    improve scroll in this branch

--- views/js/main.js
+++ views/js/main.js
@@ -507,10 +507,14 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  // define outside loop
+  var bodyInit = document.body.scrollTop / 1250;
+
+
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin( bodyInit + (i % 5) );
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
-  }
+   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
   // Super easy to create custom metrics.
